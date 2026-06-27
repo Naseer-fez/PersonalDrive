2e57dcbc093b0608b5f67656b995e54ef2c21c31
Naseer <feznaseer777@gmail.com>
2026-06-27 19:22:42 +0530

    Fixed Path bugs

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ from flask import Flask,request,jsonify
 import os 
 from dotenv import load_dotenv
 from utils.FileHelpers import CreateDir
-
+from pathlib import Path
 load_dotenv()
 app = Flask(__name__)
 
+
+##FILESAVE IN THE SERVER
 @app.route("/<int:Userid>",defaults={"directory":None},methods=["GET","POST"]) 
 @app.route("/<int:Userid>/<string:directory>",methods=["GET","POST"]) 
 def home(Userid,directory):
     Stream=request.environ["wsgi.input"]
     Filename=os.getenv("DestinationFolder")
-    Recivedfile=request.files['filename'].filename
-    PATH=CreateDir(Userid,directory,Filename)
-    if  PATH==0:
+    Recivedfile=str(request.files['filename'].filename)
+    Tosave=CreateDir(Userid=Userid,Directory=directory,Filename=Recivedfile)
+    if  Tosave==0:
         return jsonify({"return":"Some Error in Creating the Directory"}),401
-    with open (PATH,"ab") as File:
+    with open (file=Path(Tosave),mode="ab") as File:
         while True:
             Chunk=request.stream.read((1024)*16) #16MB
             if not Chunk :
@@ -23,5 +25,7 @@ def home(Userid,directory):
             File.write(Chunk)
     # with open() as File:
     return jsonify({"return":"File Saved in the server"}),200
+
+
 if __name__ == "__main__":
     app.run(debug=True)
--- utils/FileHelpers.py
+++ utils/FileHelpers.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def CreateDir(Userid,Directory,Filename):
-    # DIR=Path(os.getenv("DestinationFolder")) #FEZ
     try:
-        DIR=Path(r"D:\CODE\PYTHON\CODE\Projects\Personaldrive\test")
+        DIR=Path(os.getenv("DestinationFolder"))   #FEZ
+        # DIR=Path(r"D:\CODE\PYTHON\CODE\Projects\Personaldrive\test")
         #Check if Userid exists
         Userid=str(Userid)    
         if Directory is None:
@@ -14,13 +14,16 @@ def CreateDir(Userid,Directory,Filename):
         else:
             DIR=os.path.join(DIR,Userid,Directory)
         # print(DIR)
-        Path(DIR).mkdir(parents=True, exist_ok=True)
-        return os.path.join(DIR,Filename)
+        Path(DIR).mkdir(parents=True, exist_ok=True) #Dir is created 
+        Filename=Path(Filename).name
+        # print(Filename)
+        return Path(os.path.join(DIR,Filename))
     except Exception as e:
         return 0
+
     
 if __name__=="__main__":
-    print(CreateDir(Directory=None,Filename="1",Userid="1"))
+    print(CreateDir(Directory=None,Filename=r"Live.mp4",Userid="1"))
     import shutil
 #     shutil.rmtree(
 #     os.path.join(
