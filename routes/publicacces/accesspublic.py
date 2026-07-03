8a707a6df095131dc932ae97fdd17d2c9c4f3eba
Naseer <feznaseer777@gmail.com>
2026-07-03 23:24:30 +0530

    Add pathencoding for better path managment

--- routes/publicacces/accesspublic.py
+++ routes/publicacces/accesspublic.py
@@ -1,6 +1,6 @@
 from flask import Blueprint,jsonify,Response
 from utils.Storage import get_storage
-from .tookengeneration import verification,verify_token,maplink
+from .tookengeneration import verification,verify_token,maplink,path64
 from utils.FileHelpers import filedetails
 from dotenv import load_dotenv
 import os
@@ -13,13 +13,14 @@ def Home(userid,filesharing,time,tooken):
     userid=str(userid)
     timeverfication=verification(time)
     if timeverfication == 0:
-        return jsonify({"return":"link expired"}),400
-    data=maplink(userid=userid,filepath=filesharing,exptime=time,change=0)
+        return jsonify({"return":"link expired"}),400   
+    data = maplink(userid=userid, filepath=filesharing, exptime=time, change=0, encode=None)
     if not verify_token(data=data,token=tooken):
         return jsonify({"return":"the tooken which you have recived is invalid"}),400
+    decoded_filepath = path64(filesharing, encode=0)
     #direclty copied from downlaods
     #can redirect to the link need to think about that    
-    filepath,filesize,filetype=filedetails((userid),filesharing)
+    filepath,filesize,filetype=filedetails((userid),decoded_filepath)
     if filepath is None:
         return jsonify({"retutn":"WrongFile Inputed Tryagain"}),400
     headerdata={"filesize":filesize,"filetype":filetype}
