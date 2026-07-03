d9e81560f227af5a0d859781df5917dcc817a1af
Naseer <feznaseer777@gmail.com>
2026-07-03 22:07:37 +0530

    Implemented Public file sharing

---
+++ routes/publicacces/accesspublic.py
@@ -0,0 +1,34 @@
+from flask import Blueprint,jsonify,Response
+from utils.Storage import get_storage
+from .tookengeneration import verification,verify_token,maplink
+from utils.FileHelpers import filedetails
+from dotenv import load_dotenv
+import os
+publicbp=Blueprint("public",__name__)
+load_dotenv
+
+Fileoperation=get_storage()
+@publicbp.route("/share/<int:userid>/<string:filesharing>/<int:time>/<string:tooken>",methods=["GET"])
+def Home(userid,filesharing,time,tooken):
+    userid=str(userid)
+    timeverfication=verification(time)
+    if timeverfication == 0:
+        return jsonify({"return":"link expired"}),400
+    data=maplink(userid=userid,filepath=filesharing,exptime=time,change=0)
+    if not verify_token(data=data,token=tooken):
+        return jsonify({"return":"the tooken which you have recived is invalid"}),400
+    #direclty copied from downlaods
+    #can redirect to the link need to think about that    
+    filepath,filesize,filetype=filedetails((userid),filesharing)
+    if filepath is None:
+        return jsonify({"retutn":"WrongFile Inputed Tryagain"}),400
+    headerdata={"filesize":filesize,"filetype":filetype}
+    SIZE=os.getenv("size") or 5
+    return Response(Fileoperation.readdata(filename=filepath,Sizeofdata=SIZE),
+                    mimetype=filetype,headers=headerdata)
+    
+    
+    
+
+
+        
---
+++ routes/publicacces/setpublic.py
@@ -0,0 +1,28 @@
+from flask import Blueprint,jsonify
+from utils.Storage import get_storage
+from utils.acceptjson import getjson
+from .tookengeneration  import generatelink,maplink 
+from dotenv import load_dotenv
+import os
+setpublicbp=Blueprint("setpublic",__name__)
+
+Fileoperation=get_storage()
+@setpublicbp.route("/access/<int:userid>/",methods=["GET"])
+@getjson
+def Home(userid,data):
+    userid=str(userid)
+    filepath=data.get("filepath")
+    expiredata=604800 if data.get("time") is None else data.get("time") #week of default
+    if filepath is None:
+        return jsonify({"return":"no filepath sent"}),400
+    tooken=maplink(userid=userid,filepath=filepath,exptime=expiredata)
+    link=generatelink(data=tooken)
+    base=os.getenv("backend")
+    # link=f"{base}/{link}" #rember to take thsi off
+    return jsonify({"return":str(link)}),200
+    
+
+
+
+
+    
