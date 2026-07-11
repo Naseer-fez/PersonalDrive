ab5ab76064eab9e6b03075be960c0f950d903aea
Naseer <feznaseer777@gmail.com>
2026-07-11 15:51:28 +0530

    Configured Rate Limiter

--- utils/ratelimiter.py
+++ utils/ratelimiter.py
@@ -4,6 +4,13 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 path=os.getenv("ratelimiter")
+
+ALLOW={
+    "FileUpload.home",
+    "Downloadbp.home",
+    "folderupload.home"
+    
+    }
 def enableratelimiter(app):
     
     @app.before_request
@@ -12,10 +19,12 @@ def enableratelimiter(app):
             return
         ip = request.remote_addr
         endpoint = request.endpoint or request.path
+        if endpoint in ALLOW:
+            return
         endpoint=endpoint.replace("/", "")
         key=f"{ip}:{endpoint}"
         waittime=RL(IP_Adrs=key,FolderPath=path,Filename=endpoint.strip("/"),Format=None,
-                    CooldownTime=20,ResetTime=10,AllowedFreq=25)
+                    CooldownTime=20,ResetTime=10,AllowedFreq=50)
         if waittime!=1:
             return jsonify({"return":f"too many requests!!, wait for {waittime}sec","waittime":int(waittime)}),429
         return
