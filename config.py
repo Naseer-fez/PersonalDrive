eaa56227f3e27ed15d61e38d3573e375485207f2
Naseer <feznaseer777@gmail.com>
2026-07-13 22:12:45 +0530

    Configs Files

--- config.py
+++ config.py
@@ -2,14 +2,18 @@ import json
 
 class Config:
     def __init__(self):
+        self.path="config.json"
         self.reload()
-        
     def reload(self):
-        with open("config.json") as file:
+        with open(self.path) as file:
             self.data=json.load(file)
             
     def get(self, key, default=None):
         return self.data.get(key, default)
+    def set(self, key, value):
+        self.data[key] = value
+        with open(self.path, "w") as file:
+            json.dump(self.data, file, indent=4)
         
 config = Config()
 
