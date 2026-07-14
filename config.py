c98acffe00e782153630786ee2c37387d5cb5c3d
Naseer <feznaseer777@gmail.com>
2026-07-14 20:04:05 +0530

    Fixed file not found error

--- config.py
+++ config.py
@@ -5,9 +5,13 @@ class Config:
         self.path="config.json"
         self.reload()
     def reload(self):
-        with open(self.path) as file:
-            self.data=json.load(file)
-            
+        try:
+            with open(self.path) as file:
+                self.data=json.load(file)
+        except FileNotFoundError :
+            with open(self.path,"w") as file:
+                self.data={}
+                json.dump(self.data,file,indent=4)
     def get(self, key, default=None):
         return self.data.get(key, default)
     def set(self, key, value):
